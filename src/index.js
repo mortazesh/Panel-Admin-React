35d69728e911d335f8aa2f3bf23ee7178bbae14b
unknown <mortazesh1380@gmail.com>
2022-06-20 06:53:50 -0700

    add-font-awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
